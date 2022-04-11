22adffee79f1d30524c0a0c9e337328926ff1b93
The-Coding-Kid <veerrohitv08@gmail.com>
2022-04-11 06:09:35 -0700

    added prisma client import, will start actually working on frontend soon

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// require(dotenv.config());
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 app.use(cors());
 

7c48e54dfd863a217231105a5f208b7d2e94f772
The-Coding-Kid <veerrohitv08@gmail.com>
2022-05-11 09:23:07 -0700

    following a tutorial

--- server/apollo-server.ts
+++ server/apollo-server.ts
@@ -1,7 +1,4 @@
-import { PrismaClient } from "@prisma/client";
 const { ApolloServer, gql } = require("apollo-server");
-
-const prisma = new PrismaClient()
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against

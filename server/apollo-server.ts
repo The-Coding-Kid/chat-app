77fbce0491e3044051047485d280cfb0ea4cd9e0
The-Coding-Kid <veerrohitv08@gmail.com>
2022-05-11 08:55:06 -0700

    added some new packages

--- server/apollo-server.ts
+++ server/apollo-server.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 const { ApolloServer, gql } = require("apollo-server");
 
+const prisma = new PrismaClient()
+
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.

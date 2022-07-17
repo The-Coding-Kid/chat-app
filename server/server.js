730748cf0f9b4e1cd28c8f8506f56fa04ffdb21e
The-Coding-Kid <veerrohitv08@gmail.com>
2022-07-17 10:52:28 -0700

    created new schemas for registering and posts, need to update this later

--- server/server.js
+++ server/server.js
@@ -4,10 +4,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
 require("dotenv").config();
+const router = require("express").Router();
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+
+const register = require("./routes/register");
+const posts = require("./routes/posts");
 
 // Connect to MongoDB
 mongoose
@@ -27,3 +31,6 @@ const port = process.env.PORT || 5001;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
+
+app.use("/api/register", register);
+app.use("api/posts", posts);

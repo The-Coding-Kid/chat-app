a4a1e96f5e6a10841ac9e925e8ab1c4fb08376be
The-Coding-Kid <veerrohitv08@gmail.com>
2022-07-23 15:46:00 -0700

    added firebase_init to gitignore

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useContext } from "react";
 import { View, StyleSheet, Text, Image, Keyboard, TouchableWithoutFeedback } from "react-native";
 import { Title, Button } from "react-native-paper";
+import {db} from "../firebase_init";
 
 const HomeScreen = ({ navigation: { navigate } }) => { 
+    const Create = () => {
+        const myDoc = 
+    }
     //TODO: This is a placeholder for the HomeScreen.
     //TODO: Replace this with the actual HomeScreen.
     return (

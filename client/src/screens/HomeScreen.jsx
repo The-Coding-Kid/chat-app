282122d1b97ccd6a30915edb260503ed5ecec908
The-Coding-Kid <veerrohitv08@gmail.com>
2022-07-23 16:09:12 -0700

    need to try this code on aravinds computer

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,20 +1,41 @@
 import React, { useState, useContext } from "react";
-import { View, StyleSheet, Text, Image, Keyboard, TouchableWithoutFeedback } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { Title, Button } from "react-native-paper";
-import {db} from "../firebase_init";
-import { doc, setDoc } from 'firebase/firestore';
+import { db } from "../../firebase_init";
+import { doc, setDoc } from "firebase/firestore";
 
-const HomeScreen = ({ navigation: { navigate } }) => { 
-    const Create = () => {
-        const myDoc = doc(db, "users", "user1");
-    }
-    //TODO: This is a placeholder for the HomeScreen.
-    //TODO: Replace this with the actual HomeScreen.
-    return (
-        <View>
-            <Title>Home</Title>
-        </View>
-    )
-}
+const HomeScreen = ({ navigation: { navigate } }) => {
+  const Create = () => {
+    const myDoc = doc(db, "users", "user1");
+    const docData = {
+      firstName: "John",
+      lastName: "Doe",
+      email: "johndoe@gmail.com",
+      password: "123456",
+    };
+    setDoc(myDoc, docData)
+      .then(() => {
+        console.log("Document created");
+      })
+      .err((err) => {
+        console.log(err);
+      });
+  };
+  //TODO: This is a placeholder for the HomeScreen.
+  //TODO: Replace this with the actual HomeScreen.
+  return (
+    <View>
+      <Title>Home</Title>
+      <Button onPress={Create}>Create</Button>
+    </View>
+  );
+};
 
 export default HomeScreen;

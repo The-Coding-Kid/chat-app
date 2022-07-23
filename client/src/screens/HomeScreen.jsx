a2302c51fbfc8832b1d0ca5add33ce00802f0751
The-Coding-Kid <veerrohitv08@gmail.com>
2022-07-23 15:48:30 -0700

    added some packages:

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -2,10 +2,11 @@ import React, { useState, useContext } from "react";
 import { View, StyleSheet, Text, Image, Keyboard, TouchableWithoutFeedback } from "react-native";
 import { Title, Button } from "react-native-paper";
 import {db} from "../firebase_init";
+import { doc, setDoc } from 'firebase/firestore';
 
 const HomeScreen = ({ navigation: { navigate } }) => { 
     const Create = () => {
-        const myDoc = 
+        const myDoc = doc(db, "users", "user1");
     }
     //TODO: This is a placeholder for the HomeScreen.
     //TODO: Replace this with the actual HomeScreen.

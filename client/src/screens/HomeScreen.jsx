d91f2f21eb85fc6f1ec3b8d8c9cfb461af6d96da
Kavitha Palanisamy <archita@kavithas-air.lan>
2022-07-24 14:32:26 -0700

    I got the adding stuff to the database with errors popping up

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -9,23 +9,48 @@ import {
 } from "react-native";
 import { Title, Button } from "react-native-paper";
 import { db } from "../../firebase_init";
+import {
+  collection,
+  addDoc,
+  query,
+  where,
+  getDocs,
+  deleteDoc,
+  doc,
+  setDoc,
+} from "firebase/firestore";
 
 const HomeScreen = ({ navigation: { navigate } }) => {
+  // const Create = () => {
+  //   const myDoc = doc(db, "users", "user1");
+  //   const docData = {
+  //     firstName: "John",
+  //     lastName: "Doe",
+  //     email: "johndoe@gmail.com",
+  //     password: "123456",
+  //   };
+  //   setDoc(myDoc, docData)
+  //     .then(() => {
+  //       console.log("Document created");
+  //     })
+  //     .err((err) => {
+  //       console.log(err);
+  //     });
+  // };
+
   const Create = () => {
-    const myDoc = doc(db, "users", "user1");
     const docData = {
       firstName: "John",
       lastName: "Doe",
       email: "johndoe@gmail.com",
       password: "123456",
     };
-    setDoc(myDoc, docData)
-      .then(() => {
-        console.log("Document created");
+    const NameCollectionRef = collection(db, "users");
+    addDoc(NameCollectionRef, { docData })
+      .then((response) => {
+        console.log(response.id);
       })
-      .err((err) => {
-        console.log(err);
-      });
+      .catch((error) => console.log(error.message));
   };
   //TODO: This is a placeholder for the HomeScreen.
   //TODO: Replace this with the actual HomeScreen.

0584063cbb354068ac927b8adf0b52e47bcad40d
Kavitha Palanisamy <archita@kavithas-air.lan>
2022-07-24 19:04:55 -0700

    signout

--- client/src/screens/AccountScreen.jsx
+++ client/src/screens/AccountScreen.jsx
@@ -1,13 +1,33 @@
 import React from "react";
-import { View, StyleSheet, Text, Image, Keyboard, TouchableWithoutFeedback } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { Title, Button } from "react-native-paper";
+import { auth } from "../../firebase_init";
+import { signOut } from "firebase/auth";
 
-const AccountScreen = ({ navigation: { navigate } }) => { 
-    return (
-        <View>
-            <Title>Account</Title>
-        </View>
-    )
-}
+const AccountScreen = ({ navigation: { navigate } }) => {
+  const SignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("signed in");
+        navigate("Sign In");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <View>
+      <Title>Account</Title>
+      <Button onPress={SignOut()}>Sign out</Button>
+    </View>
+  );
+};
 
 export default AccountScreen;

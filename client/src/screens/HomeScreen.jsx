794e19ff838769a15d33e6cc68e2c7fa14b8556f
Kavitha Palanisamy <archita@kavithas-air.lan>
2022-07-25 07:46:00 -0700

    made it look better

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -55,12 +55,19 @@ const HomeScreen = ({ navigation: { navigate } }) => {
   //TODO: This is a placeholder for the HomeScreen.
   //TODO: Replace this with the actual HomeScreen.
   return (
-    <View>
+    <View style={styles.container}>
       <Title>Home</Title>
       <Text>Email: {auth.currentUser?.email}</Text>
       <Button onPress={Create}>Create</Button>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+  },
+});
 
 export default HomeScreen;
